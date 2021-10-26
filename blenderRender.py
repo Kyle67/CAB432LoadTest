48f11186654e4141438eff01f38ca5e22725b712
Kyle67 <kyle67@hotmail.com.au>
2021-10-26 10:42:38 +1000

    updated command

--- blenderRender.py
+++ blenderRender.py
@@ -7,7 +7,7 @@ blenderRender = Flask(__name__)
 
 @blenderRender.route("/")
 def hello():
-    os.system("sudo blender -b test.blend");
+    os.system("sudo blender -b test.blend -x 1 -o //render -a");
     return "Blender World!"
 
 if __name__ == "__main__":
